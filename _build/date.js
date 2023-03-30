287d6e96cb8577f082a463f96e24cc4c87fc5859
Anthony Ciccarello <aciccarello@sitepen.com>
2023-03-30 10:44:49 -0700

    add resume

--- _build/date.js
+++ _build/date.js
@@ -1,13 +1,43 @@
 const pluginRss = require('@11ty/eleventy-plugin-rss');
 
-function yearsSince(date) {
+function yearsSince(date, endInput) {
+	let includeMonths = false;
+	let end;
+	if (endInput) {
+		// Resume wants months
+		includeMonths = true;
+		end = new Date(endInput);
+	} else {
+		end = new Date();
+	}
+
 	const start = new Date(date);
-	const milliseconds = new Date().getTime() - start.getTime();
-	const years = new Date(milliseconds).getFullYear() - 1970;
-	return String(years);
+	const milliseconds = end.getTime() - start.getTime();
+	const duration = new Date(milliseconds);
+	const years = duration.getFullYear() - 1970;
+	let months = duration.getMonth(); // Intentionally 0 indexed
+	const days = duration.getDate();
+
+	if (includeMonths) {
+		if (days > 15) {
+			months++;
+		}
+		const monthText =
+			months > 0 ? `${months} month${months > 1 ? 's' : ''}` : '';
+		const yearText =
+			years > 0 ? `${years} year${years > 1 ? 's' : ''}` : '';
+		return [yearText, monthText].filter(Boolean).join(' ');
+	}
+
+	// Round up at 10 months
+	return String(months > 9 ? years : years + 1);
 }
 
 const dateFormat = new Intl.DateTimeFormat([], { dateStyle: 'medium' });
+const monthFormat = new Intl.DateTimeFormat([], {
+	year: 'numeric',
+	month: 'long',
+});
 const dateTimeFormat = new Intl.DateTimeFormat([], {
 	dateStyle: 'medium',
 	timeStyle: 'short',
@@ -23,17 +53,28 @@ const dateTimeFormat = new Intl.DateTimeFormat([], {
 module.exports = function (eleventyConfig) {
 	eleventyConfig.addGlobalData('buildTime', () => new Date());
 	eleventyConfig.addFilter('yearsSince', yearsSince);
-	eleventyConfig.addFilter('formatHumanDate', (dateInput, trimDate) => {
+	eleventyConfig.addFilter('formatHumanDate', (dateInput, accuracy) => {
 		const date = new Date(dateInput);
-		if (trimDate || date.toISOString().endsWith('T00:00:00.000Z')) {
+		if (accuracy === 'month') {
+			return monthFormat.format(date);
+		}
+		if (
+			accuracy === 'day' ||
+			date.toISOString().endsWith('T00:00:00.000Z')
+		) {
 			return dateFormat.format(date);
 		}
 		return dateTimeFormat.format(date) + ' UTC';
 	});
-	eleventyConfig.addFilter('formatMachineDate', (date) => {
+	eleventyConfig.addFilter('formatMachineDate', (date, accuracy) => {
 		const stringDate = pluginRss.dateToRfc3339(date);
 		const [datePart, timePart] = stringDate.split('T');
-		// If there is no time, use the simpler YYYY-MM-DD format
-		return timePart === '00:00:00Z' ? datePart : stringDate;
+		if (accuracy === 'month') {
+			return datePart.substring(0, 6);
+		}
+		if (accuracy === 'day' || timePart === '00:00:00Z') {
+			return datePart;
+		}
+		return stringDate;
 	});
 };
---
+++ _data/resume.js
@@ -0,0 +1,171 @@
+// @ts-check
+
+/** @typedef Experience
+ * @property {string} companyName
+ * @property {string} [companyUrl] Link to company website
+ * @property {string} position Job title
+ * @property {string} location Geographic location (or "Remote")
+ * @property {Date} startDate Date object of start (month approximation)
+ * @property {Date} [endDate] Date object of start (month approximation)
+ * @property {string} [durationText] Override of the default years/months text
+ * @property {string} summary 1 or 2 sentence summary of the work involved
+ * @property {string[]} achievements Individual bullet points
+ * @property {string[]} [moreAchievements] Individual bullet points that are hidden in concise view
+ * @property {string[]} [skills] List of skills used
+ */
+
+/** @type {Experience[]} */
+const experience = [
+	{
+		companyName: 'SitePen',
+		companyUrl: 'https://www.sitepen.com/',
+		position: 'Software Engineer',
+		location: 'Remote',
+		startDate: new Date('2018-08-21'),
+		summary:
+			'Full-stack Software development and consulting work for multiple clients.',
+		achievements: [
+			'Acted as team lead on Angular application development for a financial services corporation.',
+			'Developed a large React application for a multi-national accounting org with agile/rapid release flow.',
+			'Worked on and with AWS Lambda, DynamoDB, and Elastic Beanstalk back-ends using Express.js and Java Spring.',
+			'Lead test automation implementation and 6-month junior developer coaching program.',
+		],
+		skills: [
+			'Angular',
+			'React',
+			'JavaScript',
+			'TypeScript',
+			'AWS Lambda',
+			'DynamoDB',
+			'Elastic Beanstalk',
+			'Agile',
+			'Scrum',
+			'Java',
+			'Spring',
+			'Express.js',
+			'Team Leadership',
+			'Coaching',
+			'CI/CD',
+			'Jenkins',
+		],
+	},
+	{
+		companyName: 'DISH Network',
+		companyUrl: 'https://www.dish.com/',
+		position: 'Software Engineer',
+		location: 'Denver, CO',
+		startDate: new Date('2018-05-15'),
+		endDate: new Date('2018-08-15'),
+		summary: 'Contract position with central customer data API team.',
+		achievements: [
+			'Develop Java REST API microservices using Spring Boot in XP/TDD environment',
+			'Help inform API and architectural designs for development team',
+		],
+		skills: [
+			'Java',
+			'Spring',
+			'REST',
+			'MongoDB',
+			'Extreme Programming',
+			'Agile',
+			'Rally',
+		],
+	},
+	{
+		companyName: 'Teradata',
+		companyUrl: 'https://www.teradata.com/',
+		position: 'Software/Sys Eng Anlyst, IT Services',
+		location: 'Dayton, OH/Remote',
+		startDate: new Date('2014-06-15'),
+		endDate: new Date('2018-04-15'),
+		summary:
+			'Developed and maintained internal HR applications for 10,000 employee company. Applications I helped maintain included payroll, sales compensation, and benefits tools.',
+		achievements: [
+			'Designed and lead new AngularJS/ag-grid web application deployment/testing',
+			'Java Spring and Oracle SQL database development plus JSP and Classic ASP page maintenance',
+		],
+		skills: [
+			'AngularJS',
+			'ag-grid',
+			'Java',
+			'Spring',
+			'Oracle SQL',
+			'JSP',
+			'Classic ASP',
+		],
+	},
+	{
+		companyName: 'Covenant Kitchens & Baths',
+		position: 'Web Developer/Systems Technician/CAD Draftsman',
+		location: 'Westbrook, CT/Remote',
+		startDate: new Date('2011-05-15'),
+		endDate: new Date('2021-01-15'),
+		summary:
+			'Range of technical work for Kitchen & Bath design firm. CAD Drafting in-person and web administration later as freelance.',
+		achievements: [
+			'Complete website revamp, G-Suite administration, and SEO optimization',
+		],
+		moreAchievements: [
+			'Produced 2D & 3D drawings in AutoCAD and SketchUp',
+			'Transferred email system to Google Apps',
+			'Provided website and technical support on a contract basis',
+		],
+		skills: [
+			'Web design',
+			'System Administration',
+			'SEO',
+			'GSuite',
+			'AutoCAD',
+			'SketchUp',
+		],
+	},
+	{
+		companyName: 'Alstom Power',
+		position:
+			'Information Systems Coordinator/Business Intelligence Intern',
+		location: 'Hartford, CT',
+		startDate: new Date('2012-07-15'),
+		endDate: new Date('2013-08-15'),
+		durationText: 'Summers',
+		summary: 'Summer internship with large ',
+		achievements: [
+			'Automated manual data entry into SAP using VBScript, speeding up migration',
+		],
+		skills: [
+			'SAP',
+			'BusinessObjects',
+			'Hyperion',
+			'Excel Macros',
+			'VBScript',
+		],
+	},
+];
+
+module.exports = {
+	summary: `
+		Software Engineer experienced in modern JavaScript front-end frameworks and back-end REST APIs.
+		Knowledgeable in development best practices, automated testing, and agile methodologies.
+		Seeking a remote front end or full stack engineer position building high-quality websites regardless of technology.
+	`,
+	topSkills: [
+		'JavaScript',
+		'TypeScript',
+		'Angular',
+		'React',
+		'Java',
+		'HTML5',
+		'CSS',
+		'Node.js',
+		'AWS',
+		'Maven',
+		'Git',
+		'Automated Testing',
+	],
+	allSkills: Array.from(new Set(experience.map(({ skills }) => skills))),
+	experience,
+	personal: [
+		'Spent 6 months traveling around Latin America learning Spanish.',
+		'Member of the <a href="https://indieweb.org">IndieWeb</a> community working on interoperable, personal websites.',
+		'Enjoys <a href="/posts/tags/birds/">birding</a>, gardening and playing disc sports',
+	],
+};
