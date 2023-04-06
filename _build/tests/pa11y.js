4e7263de84961f0a9a9df74e08cf67d1415d1538
Anthony Ciccarello <aciccarello@sitepen.com>
2023-04-05 23:10:04 -0700

    add accessibility tests
    closes #9

---
+++ _build/tests/pa11y.js
@@ -0,0 +1,30 @@
+const testData = require('../../_data/test.json');
+const baseUrl = 'http://localhost:8080';
+
+/**
+ * Pa11y accesibility testing configuration.
+ * https://github.com/pa11y/pa11y-ci
+ *
+ * See axe docs for rule names when ignoring axe rules
+ * https://github.com/dequelabs/axe-core/blob/develop/doc/rule-descriptions.md
+ */
+module.exports = {
+	// defaults: { runners: ['axe'] },
+	urls: [
+		'/',
+		'/search/',
+		'/links/',
+		{ url: '/resume/', ignore: ['link-in-text-block'] },
+		'/posts/',
+		'/photos/',
+		'/foster/',
+		'/colophon/',
+		'/pay/',
+		...Object.values(testData.posts),
+	].map((path) => {
+		if (typeof path === 'string') {
+			return baseUrl + path;
+		}
+		return { ...path, url: baseUrl + path.url };
+	}),
+};
